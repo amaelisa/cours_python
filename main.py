0b00ccab46c11774e098230a37472087a6b5f072
Ama Elysa <amaelisa42@gmail.com>
2026-09-11 18:00:25 +0100

    Ajout des variables et affichage du profil

---
+++ main.py
@@ -0,0 +1,17 @@
+#1. texte (String) toujours en guillemets
+nom = "Amaboua"
+role = "Future Ingenieur en Genie Logiciel"
+
+#2. Nombre entier (Integer) sans guillemets
+age = 21
+
+#3. Nombre a virgule (float) utilise un point pas une virgule 
+note_moyenne = 16.5
+
+#4. Booleen(Boolean) est vrai ou faux (True / Flase avec majuscule)
+est_etudiante = True
+
+# afficher les informations dans la console
+print("bonjour, je m'appelle", nom)
+print("mon role :", role)
+print("age", 21, "ans")
